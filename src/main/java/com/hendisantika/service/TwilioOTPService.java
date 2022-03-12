504339f2d6b0650ea72f9afcbdc07ea434d158f7
Hendi Santika <hendisantika@gmail.com>
2022-03-13 06:12:34 +0700

    #9 - Add sendOTPForPasswordReset in TwilioOTPService

--- src/main/java/com/hendisantika/service/TwilioOTPService.java
+++ src/main/java/com/hendisantika/service/TwilioOTPService.java
@@ -1,8 +1,14 @@
 package com.hendisantika.service;
 
 import com.hendisantika.config.TwilioConfig;
+import com.hendisantika.dto.OtpStatus;
+import com.hendisantika.dto.PasswordResetRequestDto;
+import com.hendisantika.dto.PasswordResetResponseDto;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,7 +25,28 @@ import java.util.Map;
 @Service
 public class TwilioOTPService {
     Map<String, String> otpMap = new HashMap<>();
+
     @Autowired
     private TwilioConfig twilioConfig;
 
+    public Mono<PasswordResetResponseDto> sendOTPForPasswordReset(PasswordResetRequestDto passwordResetRequestDto) {
+
+        PasswordResetResponseDto passwordResetResponseDto = null;
+        try {
+            PhoneNumber to = new PhoneNumber(passwordResetRequestDto.getPhoneNumber());
+            PhoneNumber from = new PhoneNumber(twilioConfig.getTrialNumber());
+            String otp = generateOTP();
+            String otpMessage = "Dear Customer , Your OTP is ##" + otp + "##. Use this Passcode to complete your " +
+                    "transaction. Thank You.";
+            Message message = Message
+                    .creator(to, from,
+                            otpMessage)
+                    .create();
+            otpMap.put(passwordResetRequestDto.getUserName(), otp);
+            passwordResetResponseDto = new PasswordResetResponseDto(OtpStatus.DELIVERED, otpMessage);
+        } catch (Exception ex) {
+            passwordResetResponseDto = new PasswordResetResponseDto(OtpStatus.FAILED, ex.getMessage());
+        }
+        return Mono.just(passwordResetResponseDto);
+    }
 }
