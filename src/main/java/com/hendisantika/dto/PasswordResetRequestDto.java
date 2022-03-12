716a97f9f8489038f2d5ec19f69fad75c04ba783
Hendi Santika <hendisantika@gmail.com>
2022-03-13 06:10:02 +0700

    #5 - Add PasswordResetRequest DTO

---
+++ src/main/java/com/hendisantika/dto/PasswordResetRequestDto.java
@@ -0,0 +1,19 @@
+package com.hendisantika.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-twilio-sms
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/03/22
+ * Time: 06.09
+ */
+@Data
+public class PasswordResetRequestDto {
+    private String phoneNumber;
+    private String userName;
+    private String OTP;
+}
