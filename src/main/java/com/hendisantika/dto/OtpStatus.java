d7ff6b8d9384514e8f1db6c8c2c0933fbd7d012a
Hendi Santika <hendisantika@gmail.com>
2022-03-13 06:09:21 +0700

    #4 - Add OtpStatus DTO

---
+++ src/main/java/com/hendisantika/dto/OtpStatus.java
@@ -0,0 +1,15 @@
+package com.hendisantika.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-twilio-sms
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/03/22
+ * Time: 06.08
+ */
+public enum OtpStatus {
+
+    DELIVERED, FAILED
+}
