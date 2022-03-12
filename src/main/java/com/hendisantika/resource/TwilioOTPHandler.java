020f8c26606bfe3a66c39e547452b8e56844402c
Hendi Santika <hendisantika@gmail.com>
2022-03-13 06:14:59 +0700

    #13 - Add sendOTP in TwilioOTPHandler

--- src/main/java/com/hendisantika/resource/TwilioOTPHandler.java
+++ src/main/java/com/hendisantika/resource/TwilioOTPHandler.java
@@ -1,8 +1,14 @@
 package com.hendisantika.resource;
 
+import com.hendisantika.dto.PasswordResetRequestDto;
 import com.hendisantika.service.TwilioOTPService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +23,11 @@ import org.springframework.stereotype.Component;
 public class TwilioOTPHandler {
     @Autowired
     private TwilioOTPService twilioOTPService;
+
+    public Mono<ServerResponse> sendOTP(ServerRequest serverRequest) {
+        return serverRequest.bodyToMono(PasswordResetRequestDto.class)
+                .flatMap(dto -> twilioOTPService.sendOTPForPasswordReset(dto))
+                .flatMap(dto -> ServerResponse.status(HttpStatus.OK)
+                        .body(BodyInserters.fromValue(dto)));
+    }
 }
